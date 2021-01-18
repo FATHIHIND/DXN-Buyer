181ce1f3a269f1f5e46ecd468162b9462a229ceb
HIND FATHI <hind10bella.fathi@gmail.com>
2021-01-18 17:16:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 ?>
 
 <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+   <script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v9.0" nonce="iQsVFfIo">
+   </script>
       <div id="fb-root"></div>
       <script>
         window.fbAsyncInit = function() {
